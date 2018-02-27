239a5590d297bec92868f04c128902b68b51a48e
Matt <stygianvision@gmail.com>
2018-02-26 19:59:43 -0800

    Race and all tests

--- test/promise.spec.ts
+++ test/promise.spec.ts
@@ -45,7 +45,6 @@ describe('Promise', () => {
 
     it("defer should work properly with finally", () => {
         const d: IDeferred<Test> = Promise.defer<Test>();
-        const t: Test = new Test();
         expect(dummy.callback).not.toHaveBeenCalled();
         expect(dummy2.callback).not.toHaveBeenCalled();
         d.promise.then((result: Test) => {
@@ -59,5 +58,81 @@ describe('Promise', () => {
         d.reject("failed");
         expect(dummy2.callback).toHaveBeenCalled();
         expect(d.promise['result']).toBe("failed");
+    });
+
+    it("test race with reject", () => {
+        const d: IDeferred<Test> = Promise.defer<Test>();
+        const d2: IDeferred<Test> = Promise.defer<Test>();
+        const t: Test = new Test();
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        Promise.race([
+            d.promise,
+            d2.promise
+        ]).then(dummy.callback.bind(dummy), dummy2.callback.bind(dummy2));
+
+        d.reject("failed");
+        d2.resolve(t);
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).toHaveBeenCalled();
+        expect(d.promise['result']).toBe("failed");
+    });
+
+    it("test race with resolve", () => {
+        const d: IDeferred<Test> = Promise.defer<Test>();
+        const d2: IDeferred<Test> = Promise.defer<Test>();
+        const t: Test = new Test();
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        Promise.race([
+            d.promise,
+            d2.promise
+        ]).then(dummy.callback.bind(dummy), dummy2.callback.bind(dummy2));
+
+        d.resolve(t);
+        d.reject("failed");
+        expect(dummy.callback).toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        expect(d.promise['result']).toBe(t);
+    });
+
+    it("test all with resolve", () => {
+        const d: IDeferred<Test> = Promise.defer<Test>();
+        const d2: IDeferred<Test> = Promise.defer<Test>();
+        const t: Test = new Test();
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        Promise.all([
+            d.promise,
+            d2.promise
+        ]).then(dummy.callback.bind(dummy), dummy2.callback.bind(dummy2));
+
+        d.resolve(t);
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        d2.resolve(t);
+        expect(dummy.callback).toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        expect(d.promise['result']).toBe(t);
+    });
+
+    it("test all with reject", () => {
+        const d: IDeferred<Test> = Promise.defer<Test>();
+        const d2: IDeferred<Test> = Promise.defer<Test>();
+        const t: Test = new Test();
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        Promise.all([
+            d.promise,
+            d2.promise
+        ]).then(dummy.callback.bind(dummy), dummy2.callback.bind(dummy2));
+
+        d.resolve(t);
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).not.toHaveBeenCalled();
+        d2.reject("failed");
+        expect(dummy.callback).not.toHaveBeenCalled();
+        expect(dummy2.callback).toHaveBeenCalled();
+        expect(d2.promise['result']).toBe("failed");
     });
 });
